934dead0674eaf1d0c1ebafe032fce81187e947f
henryw374 <henryw374@gmail.com>
2018-07-26 19:00:17 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,24 @@
+# cljs.java.time
+
+A Clojurescript library that provides the jsr-310 (java.time) api.
+ 
+ 
+
+## Usage
+
+```
+(require '[java.time :refer [LocalDate]])
+```
+
+If you would like to be able to use `=` and `sort` with the java.time objects
+
+```
+(require 'cljs.java-time.extend-eq-and-compare)
+```
+
+## License
+
+Copyright © 2018 Widd Industries
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject cljs.java-time "0.1.0"
+  :description "jsr-310 (java.time) api in Clojurescript"
+  :url "http://github.com/henryw374/cljs.java.time"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[cljsjs/js-joda "1.6.2-0"]]
+  :profiles {:dev
+             {:source-paths ["dev"]
+              :dependencies [[org.clojure/clojure "1.9.0"]
+                             [org.clojure/clojurescript "1.10.238"]]
+              :plugins [[lein-doo "0.1.10"]]}})
