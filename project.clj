0bb673eeeb22c283f051202613324de714127804
henryw374 <henryw374@gmail.com>
2018-08-16 10:13:50 +0100

    upgrade js-joda

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cljs.java-time "0.1.1"
+(defproject cljs.java-time "0.1.2"
   :description "jsr-310 (java.time) api in Clojurescript"
   :url "http://github.com/henryw374/cljs.java-time"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cljsjs/js-joda "1.6.2-0"]]
+  :dependencies [[cljsjs/js-joda "1.9.1-0"]]
   :profiles {:dev
              {:source-paths ["dev"]
               :dependencies [[org.clojure/clojure "1.9.0"]
