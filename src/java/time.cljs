7fe57fc356a31bb58b5ec58e20cc2ec2d00977cd
Henry Widd <henryw374@gmail.com>
2018-10-31 21:40:12 +0000

    revert dynamically add getter method equivalents of existing methods - it was not fully ready

--- src/java/time.cljs
+++ src/java/time.cljs
@@ -1,86 +1,31 @@
 (ns java.time
-  (:require [cljsjs.js-joda]
-            [clojure.string :as string]))
+  (:require [cljsjs.js-joda]))
 
-; the js-joda implementation differs from java's java.time in that all the getter methods have had the 'get'
-; bit of the name removed, for example (.value foo), instead of (.getValue foo). The code in this ns does backflips to dynamically create the getter methods in order
-; to match the java implementaion
-
-(def Period)
-(def Instant)
-(def Duration)
-(def LocalDate)
-(def LocalTime)
-(def ZonedDateTime)
-(def LocalDateTime)
-(def Year)
-(def YearMonth)
-(def ZoneId)
-(def DayOfWeek)
-(def Month)
-(def Clock)
-(def ZoneOffset)
-(def ChronoUnit)
-(def ChronoField)
-(def TemporalAdjusters)
-(def Temporal)
-(def TemporalAmount)
-(def DateTimeFormatter)
-(def ResolverStyle)
-
-(def names (-> (ns-publics 'java.time)
-               (dissoc 'names)
-               keys))
-
-(defn getter-name [s]
-  (apply str "get" (string/upper-case (first s)) (rest s)))
-
-(declare getterize!)
-
-(defn do-gettering! [o]
-  ;static
-  (getterize! o)
-  ;super 1
-  (getterize! (. js/Object getPrototypeOf o))
-  ;instance
-  (getterize! (.-prototype o)))
-
-(defn getterize! [o]
-  (let [fields (set (concat (js-keys o) (js/Object.getOwnPropertyNames o)))]
-    (doseq [f fields]
-      (try
-        (let [v (goog.object/get o f)
-              getter-name (getter-name f)]
-          (when (and (fn? v) (not (fields getter-name)))
-            (goog.object/set o getter-name (fn [& args]
-                                             ; this rigmarole (as opposed to always calling v directly) 
-                                             ; is needed to call implementation of methods inherited from
-                                             ; superclass. e.g. getUnits
-                                             (this-as this
-                                               (or
-                                                 (when-let [f (goog.object/get this f)]
-                                                   (.apply f this args))
-                                                 (.apply v this args))))))
-          (when (object? v)
-            (do-gettering! v)))
-        (catch js/Error e)))))
-
-(doseq [n names]
-  (let [o (goog.object/get js/JSJoda (str n))]
-    (do-gettering! o)
-    (goog.object/set js/java.time (str n) o)))
-
-;OffsetTime is currently missing from JS-Joda 
-; 
-;(see https:// github.com / js-joda / js-joda/issues/240 and https://github.com/js-joda/js-joda/issues/165. 
-;
-;OffsetDateTime is also missing but ZonedDateTime has the same functionality
-; IOW - do not use the following 2 vars.
-(def OffsetDateTime (.. js/JSJoda -ZonedDateTime))
-(def OffsetTime (.. js/JSJoda -LocalTime))
-
-; not actually part of java.time of course, but handy for libs using this to have cljs var
+(def Period (.. js/JSJoda -Period))
+(def Instant (.. js/JSJoda -Instant))
+(def Duration (.. js/JSJoda -Duration))
+(def LocalDate (.. js/JSJoda -LocalDate))
+(def LocalTime (.. js/JSJoda -LocalTime))
+(def ZonedDateTime (.. js/JSJoda -ZonedDateTime))
+(def LocalDateTime (.. js/JSJoda -LocalDateTime))
+(def Year (.. js/JSJoda -Year))
+(def YearMonth (.. js/JSJoda -YearMonth))
+(def ZoneId (.. js/JSJoda -ZoneId))
+(def DayOfWeek (.. js/JSJoda -DayOfWeek))
+(def Month (.. js/JSJoda -Month))
+(def Clock (.. js/JSJoda -Clock))
+(def ZoneOffset (.. js/JSJoda -ZoneOffset))
+(def ChronoUnit (.. js/JSJoda -ChronoUnit))
+(def ChronoField (.. js/JSJoda -ChronoField))
+(def TemporalAdjusters (.. js/JSJoda -TemporalAdjusters))
+(def Temporal (.. js/JSJoda -Temporal))
+(def TemporalAmount (.. js/JSJoda -TemporalAmount))
 (def Date js/Date)
+(def DateTimeFormatter (.. js/JSJoda -DateTimeFormatter))
+(def ResolverStyle (.. js/JSJoda -ResolverStyle))
 
 (set! (.-valueOf Month) (fn [x] (goog.object.get Month x)))
+;; Following are not yet implemented in js-joda https://github.com/js-joda/js-joda/issues/165
+(def OffsetDateTime (.. js/JSJoda -ZonedDateTime))
+(def OffsetTime (.. js/JSJoda -LocalTime))
 
