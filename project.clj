f58eb9a844b8ac73823313c44befa87fdfeb3d20
Chee Sing Lee <lee@bigml.com>
2020-01-20 10:05:58 -0800

    add datetimeformatterbuilder

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject cljs.java-time "0.1.14"
+  :dependencies [[henryw374/js-joda "1.10.1-1"]
+                 [net.cgrand/macrovich "0.2.0"]
+                 [com.bhauman/figwheel-main "0.1.9"]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [com.bhauman/rebel-readline "0.1.1"]
+                 [com.bhauman/rebel-readline-cljs "0.1.4"]])
